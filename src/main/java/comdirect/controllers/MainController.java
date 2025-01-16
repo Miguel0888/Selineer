38a12c23bf23c0b7a885cf9adac38dd93a675431
Miguel0888 <40050873+Miguel0888@users.noreply.github.com>
2025-01-16 21:47:07 +0000

    initial for java 8

--- src/main/java/comdirect/controllers/MainController.java
+++ src/main/java/comdirect/controllers/MainController.java
@@ -1,20 +1,20 @@
 package comdirect.controllers;
 
-import comdirect.config.ComdirectConfig;
-import comdirect.services.BookmarkManager;
-import comdirect.services.BrowseService;
-import javafx.application.Platform;
+// import comdirect.config.ComdirectConfig;
+// import comdirect.services.BookmarkManager;
+// import comdirect.services.BrowseService;
+// import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebView;
-import netscape.javascript.JSObject;
-import org.springframework.beans.factory.annotation.Autowired;
+// import netscape.javascript.JSObject;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+// import org.springframework.stereotype.Controller;
 
-@Controller
+// @Controller
 public class MainController {
 
     @Value("${comdirect.ui.enable-javascript-console}")
@@ -23,8 +23,14 @@ public class MainController {
     @Value("${comdirect.ui.enable-javascript-debug}")
     boolean enableJavaScriptDebug;
 
-    @Autowired
-    private ComdirectConfig config;
+    // @Autowired
+    // private ComdirectConfig config;
+
+      // @Autowired
+    // private BrowseService browseService;
+
+    // @Autowired
+    // private BookmarkManager bookmarkManager;
 
     @FXML
     private WebView webView;
@@ -38,12 +44,6 @@ public class MainController {
     @FXML
     private ComboBox<String> bookmarkSelector;
 
-    @Autowired
-    private BrowseService browseService;
-
-    @Autowired
-    private BookmarkManager bookmarkManager;
-
     private boolean isLoading;
     private WebViewBridge bridge;
 
@@ -51,45 +51,45 @@ public class MainController {
     public void initialize() {
         // Browser-Dropdown initialisieren
         browserSelector.getItems().addAll("chromium", "firefox", "webkit", "edge");
-        browserSelector.setValue(config.getBrowser().getDefaultBrowser()); // Standardwert aus application.yml
+        // browserSelector.setValue(config.getBrowser().getDefaultBrowser()); // Standardwert aus application.yml
 
         // Bookmarks in die ComboBox laden
-        bookmarkSelector.getItems().addAll(bookmarkManager.getBookmarkNames());
+        // bookmarkSelector.getItems().addAll(bookmarkManager.getBookmarkNames());
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////
         /// WebView-Initialisierung
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        webView.getEngine().setJavaScriptEnabled(true); // Make sure JavaScript is enabled!
-        webView.getEngine().getLoadWorker().stateProperty().addListener((obs, oldState, newState) -> {
-            if (newState == javafx.concurrent.Worker.State.SUCCEEDED) {
-                // Seite wurde vollständig geladen
-                System.out.println("Seite vollständig geladen, registriere Bridge und JavaScript.");
+        // webView.getEngine().setJavaScriptEnabled(true); // Make sure JavaScript is enabled!
+        // webView.getEngine().getLoadWorker().stateProperty().addListener((obs, oldState, newState) -> {
+        //     if (newState == javafx.concurrent.Worker.State.SUCCEEDED) {
+        //         // Seite wurde vollständig geladen
+        //         System.out.println("Seite vollständig geladen, registriere Bridge und JavaScript.");
 
-                // Registriere die Bridge nur, wenn sie nicht bereits registriert ist
-                JSObject window = (JSObject) webView.getEngine().executeScript("window");
-                bridge = new WebViewBridge(this, browseService);
-                window.setMember("bridge", bridge);
-            }
-        });
-        webView.getEngine().locationProperty().addListener((obs, oldLocation, newLocation) -> {
-            if (newLocation.startsWith("bridge://")) {
-                bridge.handleBridgeRequest(newLocation);
-            }
-        });
+        //         // Registriere die Bridge nur, wenn sie nicht bereits registriert ist
+        //         JSObject window = (JSObject) webView.getEngine().executeScript("window");
+        //         bridge = new WebViewBridge(this, browseService);
+        //         window.setMember("bridge", bridge);
+        //     }
+        // });
+        // webView.getEngine().locationProperty().addListener((obs, oldLocation, newLocation) -> {
+        //     if (newLocation.startsWith("bridge://")) {
+        //         bridge.handleBridgeRequest(newLocation);
+        //     }
+        // });
 
-        if(config.getUi().isLoadHomePageAtStartup()) {
-            if (config.getUi().isAutoCloseCookieBannerAtStartup()) {
-                // Cookie-Banner schließen
-                displayHtmlInWebView(browseService.navigateToAndCloseCookieBanner(config.getUi().getUrlHome()));
-            } else {
-                // Standardseite anzeigen
-                displayHtmlInWebView(browseService.navigateTo(config.getUi().getUrlHome()));
-            }
-        }
-        if(config.getLogin().isAutoLogin()) {
-            onLoginClick(null);
-        }
+        // if(config.getUi().isLoadHomePageAtStartup()) {
+        //     if (config.getUi().isAutoCloseCookieBannerAtStartup()) {
+        //         // Cookie-Banner schließen
+        //         displayHtmlInWebView(browseService.navigateToAndCloseCookieBanner(config.getUi().getUrlHome()));
+        //     } else {
+        //         // Standardseite anzeigen
+        //         displayHtmlInWebView(browseService.navigateTo(config.getUi().getUrlHome()));
+        //     }
+        // }
+        // if(config.getLogin().isAutoLogin()) {
+        //     onLoginClick(null);
+        // }
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -97,17 +97,17 @@ public class MainController {
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     synchronized void displayHtmlInWebView(String htmlContent) {
-        if (isLoading) return;
-        isLoading = true;
-        Platform.runLater(() -> {
-            try {
-                webView.getEngine().loadContent(appendScripts(htmlContent));
-                addressBar.setText(browseService.page.url());
-            } finally {
-                // isLoading zurücksetzen, auch wenn ein Fehler auftritt
-                isLoading = false;
-            }
-        });
+        // if (isLoading) return;
+        // isLoading = true;
+        // Platform.runLater(() -> {
+        //     try {
+        //         webView.getEngine().loadContent(appendScripts(htmlContent));
+        //         addressBar.setText(browseService.page.url());
+        //     } finally {
+        //         // isLoading zurücksetzen, auch wenn ein Fehler auftritt
+        //         isLoading = false;
+        //     }
+        // });
     }
 
     private String appendScripts (String htmlContent) {
@@ -124,31 +124,31 @@ public class MainController {
 
     @FXML
     protected void onHomeClick() {
-        displayHtmlInWebView(browseService.navigateTo(config.getUi().getUrlHome()));
-        bookmarkSelector.setValue(bookmarkManager.getPageName());
+        // displayHtmlInWebView(browseService.navigateTo(config.getUi().getUrlHome()));
+        // bookmarkSelector.setValue(bookmarkManager.getPageName());
     }
 
     @FXML
     protected void onBackClick() {
-        displayHtmlInWebView(browseService.navigateBack());
-        bookmarkSelector.setValue(bookmarkManager.getPageName());
+        // displayHtmlInWebView(browseService.navigateBack());
+        // bookmarkSelector.setValue(bookmarkManager.getPageName());
     }
 
     @FXML
     public void onForwardClick(ActionEvent actionEvent) {
-        bookmarkSelector.setValue(null);
-        displayHtmlInWebView(browseService.navigateForward());
-        bookmarkSelector.setValue(bookmarkManager.getPageName());
+        // bookmarkSelector.setValue(null);
+        // displayHtmlInWebView(browseService.navigateForward());
+        // bookmarkSelector.setValue(bookmarkManager.getPageName());
     }
 
     @FXML
     protected void onAddressEntered() {
-        String url = addressBar.getText();
-        if (!url.startsWith("http://") && !url.startsWith("https://")) {
-            url = "https://" + url;
-        }
-        displayHtmlInWebView(browseService.navigateTo(url));
-        bookmarkSelector.setValue(bookmarkManager.getPageName());
+        // String url = addressBar.getText();
+        // if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        //     url = "https://" + url;
+        // }
+        // displayHtmlInWebView(browseService.navigateTo(url));
+        // bookmarkSelector.setValue(bookmarkManager.getPageName());
     }
 
     @FXML
@@ -159,55 +159,55 @@ public class MainController {
 
     @FXML
     public void onBrowserSelectionChanged(ActionEvent actionEvent) {
-        String selectedBrowser = browserSelector.getValue();
-        browseService.changeBrowser(selectedBrowser);
+        // String selectedBrowser = browserSelector.getValue();
+        // browseService.changeBrowser(selectedBrowser);
     }
 
     @FXML
     public void onRefreshClick(ActionEvent actionEvent) {
-        displayHtmlInWebView(browseService.refreshPage());
+        // displayHtmlInWebView(browseService.refreshPage());
     }
 
     @FXML
     public void onLoginClick(ActionEvent actionEvent) {
-        bookmarkSelector.setValue(null);
-        try {
-            if(config.getLogin().isUseDifferentLoginUrl())
-            {
-                if(config.getLogin().isAutoCloseCookieBanner()) {
-                    // Cookie-Banner schließen und Login-Seite anzeigen
-                    displayHtmlInWebView(browseService.navigateToAndCloseCookieBanner(config.getLogin().getUrl()));
-                } else {
-                    // Login-Seite anzeigen
-                    displayHtmlInWebView(browseService.navigateTo(config.getLogin().getUrl()));
-                }
-            }
+        // bookmarkSelector.setValue(null);
+        // try {
+        //     if(config.getLogin().isUseDifferentLoginUrl())
+        //     {
+        //         if(config.getLogin().isAutoCloseCookieBanner()) {
+        //             // Cookie-Banner schließen und Login-Seite anzeigen
+        //             displayHtmlInWebView(browseService.navigateToAndCloseCookieBanner(config.getLogin().getUrl()));
+        //         } else {
+        //             // Login-Seite anzeigen
+        //             displayHtmlInWebView(browseService.navigateTo(config.getLogin().getUrl()));
+        //         }
+        //     }
 
-            if( BrowserUtils.requestCredentialsFromUser(config))
-            {
-                // Login ausführen
-                String responseHtml = browseService.performLogin(config.getLogin().getUser(), config.getLogin().getPin());
-                displayHtmlInWebView(responseHtml);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            BrowserUtils.showError("Fehler", "Aktion fehlgeschlagen", e.getMessage());
-        }
+        //     if( BrowserUtils.requestCredentialsFromUser(config))
+        //     {
+        //         // Login ausführen
+        //         String responseHtml = browseService.performLogin(config.getLogin().getUser(), config.getLogin().getPin());
+        //         displayHtmlInWebView(responseHtml);
+        //     }
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        //     BrowserUtils.showError("Fehler", "Aktion fehlgeschlagen", e.getMessage());
+        // }
     }
 
     @FXML
     public void onBookmarkSelectionChanged(ActionEvent actionEvent) {
-        // Name des ausgewählten Bookmarks abrufen
-        String selectedBookmarkName = bookmarkSelector.getValue();
+        // // Name des ausgewählten Bookmarks abrufen
+        // String selectedBookmarkName = bookmarkSelector.getValue();
 
-        // URL zum Bookmark abrufen
-        String url = bookmarkManager.getBookmarkUrlByName(selectedBookmarkName);
+        // // URL zum Bookmark abrufen
+        // String url = bookmarkManager.getBookmarkUrlByName(selectedBookmarkName);
 
-        if (url != null) {
-            // URL im Browser öffnen
-            displayHtmlInWebView(browseService.navigateTo(url));
-        } else {
-            System.err.println("Fehler: Keine URL für das ausgewählte Bookmark gefunden.");
-        }
+        // if (url != null) {
+        //     // URL im Browser öffnen
+        //     displayHtmlInWebView(browseService.navigateTo(url));
+        // } else {
+        //     System.err.println("Fehler: Keine URL für das ausgewählte Bookmark gefunden.");
+        // }
     }
 }
